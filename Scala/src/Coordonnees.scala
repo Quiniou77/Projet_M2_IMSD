019ebaef5b330234e1ab36890268e536309f0a92
abdoulahat <57046173+abdoulahat@users.noreply.github.com>
2020-04-22 20:26:20 +0200

    Delete Coordonnees.scala

--- Scala/src/Coordonnees.scala
+++
@@ -1,8 +0,0 @@
-/**
- * Projet Scala Master 2 IMSD - 2019/2020
- * Enseignant : Mourad Karoui
- * @authors Abdou Lahat DIOP, Hassanaini Abdourahamane et Amady DIOP
- */
-class Coordonnees(var coorX: Int, var coorY: Int) {
-
-}
